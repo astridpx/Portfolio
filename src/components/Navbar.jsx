a6712c2e6d01802fa6972e5d001e67b13f1f928b
Iskyy <iskycruise01@gmail.com>
2023-07-25 15:05:58 +0700

    feat: Add scrollintoview fuction for contact page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     const about = document.getElementById("about");
     const project = document.getElementById("projects");
     const skill = document.getElementById("skill");
-    // const contact = document.getElementById("contact");
+    const contact = document.getElementById("contact");
 
     switch (id) {
       case 2:
@@ -22,6 +22,9 @@ const Navbar = () => {
         break;
       case 4:
         project.scrollIntoView({ behavior: "smooth" });
+        break;
+      case 5:
+        contact.scrollIntoView({ behavior: "smooth" });
         break;
       default:
         return null;
